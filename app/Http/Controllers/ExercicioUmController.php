8a7dfce6d7db03013acd4d7a42769288ea610f70
emillyluzia <emillyluzia416@gmail.com>
2023-08-01 15:04:18 -0300

    form request

--- app/Http/Controllers/ExercicioUmController.php
+++ app/Http/Controllers/ExercicioUmController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ExercicioUmRequest;
 use Illuminate\Http\Request;
 
 class ExercicioUmController extends Controller
 {
-    public function multiplicar(Request $request){
+    public function multiplicar(ExercicioUmRequest $request)
+    {
         $primeiraEntrada = $request->primeiro_numero;
-        $segundaEntrada = $request->segundo_numero;   
+        $segundaEntrada = $request->segundo_numero;
 
         return json_encode([
             'resultado' => $primeiraEntrada * $segundaEntrada
-        ]); 
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\ExercicioCincoController;
+use App\Http\Controllers\ExercicioDoisController;
+use App\Http\Controllers\ExercicioDozeController;
+use App\Http\Controllers\ExercicioQuatroController;
+use App\Http\Controllers\ExercicioSeteController;
+use App\Http\Controllers\ExercicioTresController;
 use App\Http\Controllers\ExercicioUmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,25 +21,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('exercicio/um', 
-[ExercicioUmController::class, 'multiplicar']);
+Route::post('exercicio/um',[ExercicioUmController::class, 'multiplicar']);
 
-Route::post('exercicio/dois', 
-[ExercicioDoisController::class, 'retornarMaior']);
+Route::post('exercicio/dois',[ExercicioDoisController::class, 'retornarMaior']);
 
-Route::post('exercicio/tres', 
-[ExercicioTresController::class, 'aritimetica']);
+Route::post('exercicio/tres',[ExercicioTresController::class, 'aritimetica']);
 
-Route::post('exercicio/quatro', 
-[ExercicioQuatroController::class, 'verificarNumero']);
+Route::post('exercicio/quatro',[ExercicioQuatroController::class, 'verificarNumero']);
 
-Route::post('exercicio/cinco', 
-[ExercicioCincoController::class, 'divisivel']);
+Route::post('exercicio/cinco',[ExercicioCincoController::class, 'divisivel']);
 
-Route::get('exercicio/sete', 
-[ExercicioSeteController::class, 'exibirNumeros']);
+Route::get('exercicio/sete',[ExercicioSeteController::class, 'exibirNumeros']);
 
-Route::get('exercicio/doze', 
-[ExercicioDozeController::class, 'mostrarNumeros']);
-
-
+Route::get('exercicio/doze',[ExercicioDozeController::class, 'mostrarNumeros']);
